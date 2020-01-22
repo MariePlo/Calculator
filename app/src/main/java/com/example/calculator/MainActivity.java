21f8e283f14966e5eb651c549543819f0f665c90
MariePlo <marieploteau22@gmail.com>
2020-01-22 21:17:10 +0100

    click

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -11,8 +11,31 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText operation;
+    EditText result;
+    Button zero;
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button add;
+    Button minus;
+    Button multiply;
+    Button divide;
+    Button equal;
+    Button clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +44,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setUpViews();
+
+    }
+
+    public void setUpViews(){
+        zero = (Button) findViewById(R.id.button0);
+        one = (Button) findViewById(R.id.button1);
+        two = (Button) findViewById(R.id.button0);
+        three = (Button) findViewById(R.id.button0);
+        four = (Button) findViewById(R.id.button0);
+        five = (Button) findViewById(R.id.button0);
+        six = (Button) findViewById(R.id.button0);
+        seven = (Button) findViewById(R.id.button0);
+        eight = (Button) findViewById(R.id.button0);
+        nine = (Button) findViewById(R.id.button0);
+        add = (Button) findViewById(R.id.button0);
+        minus = (Button) findViewById(R.id.button0);
+        multiply = (Button) findViewById(R.id.button0);
+        divide = (Button) findViewById(R.id.button0);
+        clear = (Button) findViewById(R.id.button0);
+        operation = (EditText) findViewById(R.id.operation);
+        result = (EditText) findViewById(R.id.result);
+    }
+
+    public void myClickHandler(View view) {
+
+        operation.setText("Click");
+
     }
 }
+
